00fc21895e37a4fba39ddeb8c4c11cbc3c187d39
amit.raut <amitraut11@gmail.com>
2023-07-21 18:20:51 -0500

    added files

--- s3.tf
+++ s3.tf
@@ -7,3 +7,8 @@ resource "aws_s3_bucket" "codepipeline_artifacts-new" {
   bucket = "pipeline-artifacts-davo-amit-new-terraform"
   acl    = "private"
 } 
+
+resource "aws_s3_bucket" "codepipeline_artifacts-new1" {
+  bucket = "pipeline-artifacts-davo-amit-new-terraform1"
+  acl    = "private"
+} 
