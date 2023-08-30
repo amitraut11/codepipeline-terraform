ef4a7411afd8bb00ba3746a49335920a1f64822a
amit.raut <amitraut11@gmail.com>
2023-08-30 11:40:38 -0500

    commit 3

--- pipeline.tf
+++ pipeline.tf
@@ -63,14 +63,13 @@ resource "aws_codebuild_project" "tf-image" {
     image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE" 
- 
-    environmentVariables{
-        AWS_DEFAULT_REGION = "us-east-1"
+    variables = {
+      AWS_DEFAULT_REGION = "us-east-1"
         AWS_ACCOUNT_ID = "365235792173"
         IMAGE_REPO_NAME = "helloworld"
         IMAGE_TAG = "latest"
-
     }
+    
     registry_credential{
         credential = var.dockerhub_credentials
         credential_provider = "SECRETS_MANAGER"
