085b52062b6b0fccd26fdebe49cb169a7ab4b137
amit.raut <amitraut11@gmail.com>
2023-12-09 11:51:58 -0600

    fixed gitignore

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "terraform-backend-bucket-v1"
+        bucket = "terraform-backend-bucket-v2"
         encrypt = true
         key = "terraform.tfstate"
         region = "us-east-1"
