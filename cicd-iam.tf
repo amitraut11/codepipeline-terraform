3943a0bfad44086f11df5cf061376f4f12721e04
amit.raut <amitraut11@gmail.com>
2024-01-07 18:06:07 -0600

    added commit26

--- cicd-iam.tf
+++ cicd-iam.tf
@@ -121,3 +121,16 @@ resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment-terrafor
     policy_arn  = "arn:aws:iam::aws:policy/PowerUserAccess"
     role        = aws_iam_role.tf-codebuild-role-terraform.id
 }
+
+
+//for ecs
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
